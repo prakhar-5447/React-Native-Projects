a156517603592b2db1401ab25a204be6cde642cc
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-06-12 12:50:33 +0530

    first commit

---
+++ weatherApi/App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, Input, View} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>dioasjdlaksmdl</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
