1b8cae6ed87ea6d3dd308be766d06b3c21872969
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-06-13 13:44:33 +0530

    newsApi project done

--- newsApi/App.js
+++ newsApi/App.js
@@ -6,106 +6,89 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
+  Image,
+  Linking,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [news, setNews] = useState([]);
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  const get_data = () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'd8a45534c1mshd7a265de9d84244p1379d4jsn2abfdf8e82c3',
+        'X-RapidAPI-Host': 'free-news.p.rapidapi.com',
+      },
+    };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    fetch('https://free-news.p.rapidapi.com/v1/search?q=all&lang=en', options)
+      .then(response => response.json())
+      .then(response => {
+        setNews(response.articles);
+      })
+      .catch(err => console.error(err));
   };
 
+  useEffect(() => {
+    get_data();
+  }, []);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ScrollView>
+      {news &&
+        news.map((e, i) => (
+          <View
+            key={i}
+            style={[
+              {
+                backgroundColor: 'whitesmoke',
+                margin: 10,
+                borderWidth: 1,
+                borderRadius: 20,
+                padding: 15,
+              },
+            ]}>
+            <Image
+              style={[{width: '100%', height: 200}]}
+              source={{uri: e.media}}
+            />
+            <Text
+              style={[
+                styles.text,
+                {
+                  fontSize: 30,
+                  fontWeight: 'bold',
+                  marginTop: 10,
+                  marginBottom: 5,
+                  textDecorationLine: 'underline',
+                },
+              ]}
+              onPress={() => {
+                Linking.openURL(e.link);
+              }}>
+              {e.title}
+            </Text>
+            <Text style={[styles.text]}>{e.summary}</Text>
+            <Text style={[styles.text, {marginTop: 10, textAlign: 'right'}]}>
+              - {e.authors}
+            </Text>
+          </View>
+        ))}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  text: {
+    fontSize: 15,
   },
 });
 
